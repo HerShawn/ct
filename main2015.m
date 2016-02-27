920eb31d0ada0d25328bb40edc012adc7f20d037
her shawn <hexiangman@qq.com>
2016-02-27 21:40:33 +0800

    2016/2/27
    icdar2015
    检测：
    0.5056，0.8035，0.6206
    识别：
    0.6923,0.4356,0.5348

--- main2015.m
+++ main2015.m
@@ -16,31 +16,31 @@ save_result_dir='D:\hx\edgebox-contour-neumann\contour_2015train_detection';
 num_img = length(dir_img);
 load('model_new.mat')
 load train_hog2011.mat
-for indexImg = 1:num_img
-%     if indexImg == 76||indexImg == 85
-%         continue
-%     end
+for indexImg = 5:num_img
+    %     if indexImg == 76||indexImg == 85
+    %         continue
+    %     end
     img_value = dir_img(indexImg).name;
-    img_value = img_value(5:end-4);
+    img_value = img_value(1:end-4);
     %     if img_value == '182'||img_value == '191'
     %         continue
     %     end
-    disp(['start compute ' img_value])
+    %     disp(['start compute ' img_value])
     %     img_name = ['E:\2012 文字检测\测试集\ICDAR 2011\test-textloc-gt\' dir_img(indexImg).name];
-%        img_value = '148';
-%     img_name = ['G:\数据\test-textloc\' img_value '.jpg'];
-% img_name = ['E:\数据\icdar2011\train\train-textloc\' img_value '.jpg'];
-img_name = ['E:\数据\icdar2015\Challenge2_Test_Task12_Images\' img_value '.jpg'];
+    %        img_value = '148';
+    %     img_name = ['G:\数据\test-textloc\' img_value '.jpg'];
+    % img_name = ['E:\数据\icdar2011\train\train-textloc\' img_value '.jpg'];
+    img_name = ['E:\数据\icdar2015\Challenge2_Test_Task12_Images\' img_value '.jpg'];
     img = imread(img_name);
-    
+    g=img;
     %10.14 不显示图片，数据集里255张图片太多
-%        figure;imshow(img);title('原图');
-     %
-     
+    %        figure;imshow(img);title('原图');
+    %
+    
     [h,w,~] = size(img);
-    sizeScale = ceil(sqrt(h*w/(1200*1600)));   
-%      sizeScale = (sqrt(h*w/(1200*1600)));
-%    sizeScale = 1;
+    sizeScale = ceil(sqrt(h*w/(1200*1600)));
+    %      sizeScale = (sqrt(h*w/(1200*1600)));
+    %    sizeScale = 1;
     img = imresize(img,1/sizeScale);
     img = im2double(img);
     IsSave = 0;
@@ -49,32 +49,32 @@ img_name = ['E:\数据\icdar2015\Challenge2_Test_Task12_Images\' img_value '.jpg']
     tic
     [magGrad,orientation,bw_img,cluster_label,cluster_label_big,img,neighbor_img,color] = Compute_Parameter(img,Debug,IsSave,[save_dir 'Parameter\' img_value '.mat'],RLearners, RWeights);
     toc
-%     continue
-%    load([save_dir 'Parameter\' img_value '.mat']);
+    %     continue
+    %    load([save_dir 'Parameter\' img_value '.mat']);
     [new_h, new_w, ~] = size(img);
     tic
-   [cpoint_cell_p,cpoint_cell_n] = Corresponding_point(magGrad,orientation,bw_img,cluster_label_big,IsSave,[save_dir 'CorrespPoint\' img_value '.mat']);  %对应点列表
-   toc
-%   load([save_dir 'CorrespPoint\' img_value '.mat']);
+    [cpoint_cell_p,cpoint_cell_n] = Corresponding_point(magGrad,orientation,bw_img,cluster_label_big,IsSave,[save_dir 'CorrespPoint\' img_value '.mat']);  %对应点列表
+    toc
+    %   load([save_dir 'CorrespPoint\' img_value '.mat']);
     %% 计算正方向
     tic
     [cpoint_cell,new_corresp,color_edge] = Edge_merge(bw_img,neighbor_img,cpoint_cell_p,cluster_label_big,color.hsi_p,color.rgb_p,color.pro_p,Debug,IsSave,[save_dir 'Merge_P\'],img_value);
     toc
-%    load([save_dir 'Merge_P\' img_value '.mat']);
+    %    load([save_dir 'Merge_P\' img_value '.mat']);
     feature_vector = Compute_feature(cpoint_cell,cluster_label_big,new_corresp,IsSave,[save_dir 'Feature_P\' img_value '.mat']);
-%    load([save_dir 'Feature_P\' img_value '.mat']);
-     flag_chain = Character_Pair(cpoint_cell, color_edge,cluster_label,new_corresp,IsSave,[save_dir 'Chain_P\' img_value '.mat']);
- %   load([save_dir 'Chain_P\' img_value '.mat']);
+    %    load([save_dir 'Feature_P\' img_value '.mat']);
+    flag_chain = Character_Pair(cpoint_cell, color_edge,cluster_label,new_corresp,IsSave,[save_dir 'Chain_P\' img_value '.mat']);
+    %   load([save_dir 'Chain_P\' img_value '.mat']);
     [result_p rgb_result_p,show_img_p, bw_result_p]= Classify_character(cpoint_cell, color_edge, feature_vector, flag_chain, img, model,RLearners, RWeights, IsSave,[save_dir 'Location_P\'],img_value,new_h,new_w);
     %% 计算负方向
     tic
     [cpoint_cell,new_corresp,color_edge] = Edge_merge(bw_img,neighbor_img,cpoint_cell_n,cluster_label_big,color.hsi_n,color.rgb_n,color.pro_n,Debug,IsSave,[save_dir 'Merge_N\'],img_value);
     toc
-%      load([save_dir 'Merge_N\' img_value '.mat'])
-   feature_vector = Compute_feature(cpoint_cell,cluster_label_big,new_corresp,IsSave,[save_dir 'Feature_N\' img_value '.mat']);
-%      load([save_dir 'Feature_N\' img_value '.mat']);
+    %      load([save_dir 'Merge_N\' img_value '.mat'])
+    feature_vector = Compute_feature(cpoint_cell,cluster_label_big,new_corresp,IsSave,[save_dir 'Feature_N\' img_value '.mat']);
+    %      load([save_dir 'Feature_N\' img_value '.mat']);
     flag_chain = Character_Pair(cpoint_cell, color_edge,cluster_label,new_corresp,IsSave,[save_dir 'Chain_N\' img_value '.mat']);
-%      load([save_dir 'Chain_N\' img_value '.mat']);
+    %      load([save_dir 'Chain_N\' img_value '.mat']);
     [result_n rgb_result_n,show_img_n, bw_result_n] = Classify_character(cpoint_cell, color_edge, feature_vector, flag_chain, img, model,RLearners, RWeights,IsSave,[save_dir 'Location_N\'],img_value,new_h,new_w);
     toc
     %     figure;imshow(rgb_result_p)
@@ -125,29 +125,38 @@ img_name = ['E:\数据\icdar2015\Challenge2_Test_Task12_Images\' img_value '.jpg']
         end
         result_last(logical(flag_merge),:) = [];
         
-%           figure;imshow(img);title('检测效果');
+        %           figure;imshow(img);title('检测效果');
+        result_ct_last=[];
         for i=1:size(result_last, 1)
-%          rectangle('Position',[result_last(i,1) result_last(i,2) result_last(i,3)-result_last(i,1)+1 result_last(i,4)-result_last(i,2)+1], 'EdgeColor', 'g', 'LineWidth', 2);
-         img(result_last(i,2)-1:result_last(i,2)+1,result_last(i,1):result_last(i,3)) = 1;
-         img(result_last(i,4)-1:result_last(i,4)+1,result_last(i,1):result_last(i,3)) = 1;
-         img(result_last(i,2):result_last(i,4),result_last(i,1):result_last(i,1)+2) = 1;
-         img(result_last(i,2):result_last(i,4),result_last(i,3)-2:result_last(i,3)) = 1;
-%         imwrite(img,[save_dir 'detection\' img_value '.jpg']);
-%         pt = [result_last(i,1), result_last(i,2)];
-%         wSize = [result_last(i,3)-result_last(i,1)+1,result_last(i,4)-result_last(i,2)+1];
-%         des = drawRect(img,pt,wSize,3,[128 255 0] );
-%         img=des;
-%          subplot(1,2,1)
-%         imshow(img)
-%          rectangle('Position',[result_last(i,1) result_last(i,2) result_last(i,3)-result_last(i,1)+1 result_last(i,4)-result_last(i,2)+1], 'EdgeColor', 'g', 'LineWidth', 2);
-%         subplot(1,2,2)
-%          imshow(des)
-%         F=getframe(gcf);
-%         img(F.cdata);
-          imwrite(img,[save_result_dir '\' img_value '.jpg']);
+            %          rectangle('Position',[result_last(i,1) result_last(i,2) result_last(i,3)-result_last(i,1)+1 result_last(i,4)-result_last(i,2)+1], 'EdgeColor', 'g', 'LineWidth', 2);
+            img(result_last(i,2)-1:result_last(i,2)+1,result_last(i,1):result_last(i,3)) = 1;
+            img(result_last(i,4)-1:result_last(i,4)+1,result_last(i,1):result_last(i,3)) = 1;
+            img(result_last(i,2):result_last(i,4),result_last(i,1):result_last(i,1)+2) = 1;
+            img(result_last(i,2):result_last(i,4),result_last(i,3)-2:result_last(i,3)) = 1;
+            %         imwrite(img,[save_dir 'detection\' img_value '.jpg']);
+            %         pt = [result_last(i,1), result_last(i,2)];
+            %         wSize = [result_last(i,3)-result_last(i,1)+1,result_last(i,4)-result_last(i,2)+1];
+            %         des = drawRect(img,pt,wSize,3,[128 255 0] );
+            %         img=des;
+            %          subplot(1,2,1)
+            %         imshow(img)
+            %          rectangle('Position',[result_last(i,1) result_last(i,2) result_last(i,3)-result_last(i,1)+1 result_last(i,4)-result_last(i,2)+1], 'EdgeColor', 'g', 'LineWidth', 2);
+            %         subplot(1,2,2)
+            %          imshow(des)
+            %         F=getframe(gcf);
+            %         img(F.cdata);
+            result_ct=[max(result_last(i,1),1) max(result_last(i,2),1) result_last(i,3)-result_last(i,1)  result_last(i,4)-result_last(i,2) ];
+            
+            result_ct_last=[result_ct_last ;result_ct];
+            %           imwrite(img,[save_result_dir '\' num2str(indexImg+99) '.jpg']);
         end
-        
-%         dlmwrite([save_result_dir '\' img_value '.txt'],result_last);
+        if ~isempty(result_ct_last)
+        figure(indexImg);
+        bbGt('showRes',g,result_ct_last,result_ct_last);
+        save_name=[img_value '.jpg'];
+        print(indexImg, '-dpng', save_name);
+        end
+        %         dlmwrite([save_result_dir '\' img_value '.txt'],result_last);
         dlmwrite([save_result_dir '\' img_value '.txt'], result_last, 'delimiter', ',','newline', 'pc');
         clear result_n rgb_result_n show_img_n  bw_result_n  result_p rgb_result_p show_img_p  bw_result_p
     end
